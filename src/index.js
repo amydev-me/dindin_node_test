3308abec1c9b522f448c46e8232c895b7573b5e1
Amy <ampyaephyonaing@gmail.com>
2019-09-05 20:13:46 +0630

    Clean Code

--- src/index.js
+++ src/index.js
@@ -1,49 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
+
 const models = require( '../models/index');
+const { validateRequest } = require( './Validator/treasure');
+const { NearestQuery } = require( './utils/helpers');
 
-/**
- * Validate Input Request
- * @param {Valid} fields 
- */
-const validateQuery = (fields) =>{
-    return (req, res, next) => {
-        for(const field of fields) {
-            if(!req.query[field]) { 
-                return res
-                    .status(400)
-                    .send({'status':'invalid_request',message:'Invalid request'});
-            }
-        }
-        next();
-    };
-}
-/**
- * Calculate Distance 
- * @param {*} param0 
- */
-const NearestQuery = ({latitude, longitude}) =>{
-    return ` 3959 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * 
-                    cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * 
-                    sin( radians( latitude ) ) ) `;
-}
-
-app.get('/api/nearest-gem', validateQuery(['latitude', 'longitude','distance']), async (req,res) =>{
+app.post('/api/nearest-gem',validateRequest(), async (req,res) =>{
     try{
+        const {distance, amount} = req.body;
 
-        const {distance, amount} = req.query;
-
-        /**
-         *Amount field is optional
-         */
         const criteria = amount ? [{
                             model: models.MoneyValue,
                             where: { amt: amount }
                         } ]:[];
         
-        const query = NearestQuery(req.query);
+        const query = NearestQuery(req.body);
+
         const response = await models.Treasure.findAll({
             attributes: [
                 'id',
@@ -57,8 +31,7 @@ app.get('/api/nearest-gem', validateQuery(['latitude', 'longitude','distance']),
         res.status(200).send({count:response.length, treasures:response});
     }catch(e){
         res.status(500);
-    };
+    }
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
